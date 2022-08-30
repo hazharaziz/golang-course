7f244029e83665b9b02d3685abea3c5da8180a7e
hazharaziz <hazharaziz5555@gmail.com>
2022-08-30 09:08:22 +0430

    use i/o by bufio package

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	} 
-	fmt.Println(s)
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		text := input.Text()
+		if text == "exit" {
+			break
+		} 
+		counts[text]++
+	}
 
-	s = ""
-	sep = ""
-
-	for _, arg := range os.Args[1:] {
-		s += sep + arg
-		sep = " "
-	} 
-	fmt.Println(s)
-
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
