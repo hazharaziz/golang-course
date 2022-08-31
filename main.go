b64b2582e4d917556eafc3f9464705eb7ddc0a96
hazharaziz <hazharaziz5555@gmail.com>
2022-08-31 08:16:27 +0430

    block scope variables

--- main.go
+++ main.go
@@ -3,31 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	x := 1
-	y := &x
-	fmt.Println(*y)
-	fmt.Println("address:", y)
-	*y = 2
-	fmt.Println(x)
-	fmt.Println("address:", &x)
-
-	p := 1
-	incrementByPointer(&p)
-	fmt.Println(p)
-
-	p = 1
-	incrementByValue(p)
-	fmt.Println(p)
+	x := "hello!"
+	for i := 0; i < len(x); i++ {
+		x := x[i]
+		if x != '!' {
+			x := x + 'A' - 'a'
+			fmt.Printf("%c", x) // "HELLO" (one letter per iteration)
+		}
+	}
 }
-
-
-func incrementByValue(p int) int {
-	p++
-	return p
-}
-
-func incrementByPointer(p *int) int {
-	*p++
-	return *p
-}
-
