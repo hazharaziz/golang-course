ee0cee8ba46ebeedffbb8aab9b8d18e2ed1d3e88
hazharaziz <hazharaziz5555@gmail.com>
2022-09-02 19:56:28 +0430

    level #3 exercises done

--- main.go
+++ main.go
@@ -2,20 +2,76 @@ package main
 
 import "fmt"
 
-const (
-	_ = iota
-	// kb = 1024
-	kb = 1 << (iota * 10)
-
-	// mb = kb * 1024
-	mb = 1 << (iota * 10)
-
-	// gb = mb * 1024
-	gb = 1 << (iota * 10)
-)
-
 func main() {
-	fmt.Printf("%d\t\t%b\n", kb, kb)
-	fmt.Printf("%d\t\t%b\n", mb, mb)
-	fmt.Printf("%d\t%b\n", gb, gb)
+	// exercise #1
+	// for i := 0; i <= 10000; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// exercise #2
+	// index := 1
+	// for i := 65; i <= 90; i++ {
+	// 	fmt.Println(index)
+	// 	for j := 0; j < 3; j++ {
+	// 		fmt.Printf("\t\t%#U\n", i)
+	// 	}
+	// 	index++
+	// }
+
+	// exercise #3
+	// year := 2000
+	// for year <= 2022 {
+	// 	if year > 2022 {
+	// 		break
+	// 	}
+	// 	fmt.Println(year)
+	// 	year++
+	// }
+
+	// exercise #4
+	// year := 2000
+	// for year <= 2022 {
+	// 	fmt.Println(year)
+	// 	year++
+	// }
+
+	// exercise #5
+	// for i := 10; i <= 100; i++ {
+	// 	fmt.Printf("%v\n", i%4)
+	// }
+
+	// exercise #6, #7
+	// x := 33
+	// fmt.Println("using if statement")
+	// if x < 32 {
+	// 	fmt.Println("x is less than 32")
+	// } else if x == 32 {
+	// 	fmt.Println("x is equal to 32")
+	// } else {
+	// 	fmt.Println("x is greater than 32")
+	// }
+
+	// exercise #8
+	// y := 40
+	// switch {
+	// case y < 32:
+	// 	fmt.Println("y is less than 32")
+	// case y == 32:
+	// 	fmt.Println("y is equal to 32")
+	// case y > 32:
+	// 	fmt.Println("y is greater than 32")
+	// }
+
+	// exercise #9
+	favSport := "swimming"
+	switch favSport {
+	case "football":
+		fmt.Println("1")
+	case "volleyball":
+		fmt.Println("2")
+	case "basketball":
+		fmt.Println("3")
+	case "swimming":
+		fmt.Println("4")
+	}
 }
