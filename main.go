41e5e849bd5dc0fa58f0774a378abf2b2d6bb7d0
hazharaziz <hazharaziz5555@gmail.com>
2022-09-02 11:55:30 +0430

    hands-on exercise #5

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import "fmt"
 type myType int
 
 var x myType
+var y int
 
 func main() {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 
 	x = 42
-	fmt.Println(x)
+	fmt.Println("x: ", x)
+
+	y = int(x)
+	fmt.Println("y: ", y)
 }
