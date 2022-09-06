444983ae4ee183c394fe5bc8376c207a2aa91680
hazharaziz <hazharaziz5555@gmail.com>
2022-09-06 15:31:27 +0430

    type embedding

--- main.go
+++ main.go
@@ -2,18 +2,26 @@ package main
 
 import "fmt"
 
+type employee struct {
+	person
+	employeeId int
+}
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-	p1 := person{
-		firstName: "Hazhar",
-		lastName:  "Aziz",
+	p1 := employee{
+		person: person{
+			firstName: "Hazhar",
+			lastName:  "Aziz",
+		},
+		employeeId: 10,
 	}
 
 	fmt.Println(p1)
 	fmt.Println(p1.firstName, p1.lastName)
-
+	fmt.Println(p1.person.firstName, p1.person.lastName)
 }
