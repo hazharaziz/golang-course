cf16de98cde7f247a13620371e0943704b706814
hazharaziz <hazharaziz5555@gmail.com>
2022-09-06 16:15:55 +0430

    anonymous struct

--- main.go
+++ main.go
@@ -13,15 +13,14 @@ type person struct {
 }
 
 func main() {
-	p1 := employee{
-		person: person{
-			firstName: "Hazhar",
-			lastName:  "Aziz",
-		},
-		employeeId: 10,
+	p1 := struct {
+		firstName string
+		lastName  string
+	}{
+		firstName: "Hazhar",
+		lastName:  "Aziz",
 	}
 
 	fmt.Println(p1)
 	fmt.Println(p1.firstName, p1.lastName)
-	fmt.Println(p1.person.firstName, p1.person.lastName)
 }
