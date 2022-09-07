cc3bcb54b2292cf33206de15fb286da737c1cf0c
hazharaziz <hazharaziz5555@gmail.com>
2022-09-07 21:32:46 +0430

    level #6 - exercise #9

--- main.go
+++ main.go
@@ -3,13 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	x := foo()
-	y := x()
-	fmt.Println(y)
+	numbers := []int{1, 2, 3, 4, 5, 6}
+	oddsTotal := sumOfOddNumbers(sum, numbers)
+	fmt.Println(oddsTotal)
 }
 
-func foo() func() int {
-	return func() int {
-		return 42
+func sum(numbers []int) int {
+	total := 0
+	for _, value := range numbers {
+		total += value
 	}
+	return total
+}
+
+func sumOfOddNumbers(sumCallback func(numbers []int) int, allNumbers []int) int {
+	var oddNumbers []int = make([]int, 0)
+	for _, number := range allNumbers {
+		if number%2 == 1 {
+			oddNumbers = append(oddNumbers, number)
+		}
+	}
+	sum := sumCallback(oddNumbers)
+	return sum
 }
